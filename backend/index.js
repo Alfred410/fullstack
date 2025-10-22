a6de57652a38d27bcd652551dacdb9db99e4b4de
Alfred410 <alfred.larsson@iths.se>
2025-10-22 14:19:12 +0200

    Add backend and database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,59 @@
 const express = require("express"),
   path = require("path");
+const dotenv = require("dotenv"),
+  { Client } = require("pg");
+const { request } = require("http");
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
 
 const app = express();
 
-app.get("/api", (_request, response) => {
-  response.send({ hello: "World" });
+const createTable = async () => {
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(300) NOT NULL,
+    completed BOOLEAN DEFAULT false,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+};
+createTable();
+
+app.get("/todos", async (_request, response) => {
+  const result = await client.query("SELECT * FROM todos");
+  response.json(result.rows);
+});
+
+app.post("/todos", async (request, response) => {
+  const { title } = request.body;
+  const result = await client.query(
+    "INSERT INTO todos (title) VALUES ($1) RETURNING *",
+    [title]
+  );
+  response.json(result.rows[0]);
+});
+
+app.put("/todos/:id", async (request, response) => {
+  const { id } = request.params;
+  const { completed } = request.body;
+  await client.query("UPDATE todos SET completed = $1 WHERE id = $2", [
+    completed,
+    id,
+  ]);
+  response.json({ message: "Todo updated" });
+});
+
+app.delete("/todos/:id", async (request, response) => {
+  const { id } = request.params;
+  await client.query("DELETE FROM todos WHERE id = $1", [id]);
+  response.json({ message: "Todo deleted" });
 });
 
 app.use(express.static(path.join(path.resolve(), "dist")));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,76 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const fetchTodo = async () => {
+    const res = await fetch(`/todos`);
+    const data = await res.json();
+    setTodos(data);
+  };
+
+  const addTodo = async () => {
+    if (!newTodo.trim()) return;
+    await fetch(`/todos`, {
+      method: "POST",
+      headers: { "Content-Type": "appication/json" },
+      body: JSON.stringify({ title: newTodo }),
+    });
+    setNewTodo("");
+    fetchTodo();
+  };
+
+  const toggleTodo = async (id, completed) => {
+    await fetch(`/todos/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "appication/json" },
+      body: JSON.stringify({ completed: !completed }),
+    });
+    fetchTodo();
+  };
+
+  const deleteTodo = async (id) => {
+    await fetch(`/todos/${id}`, {
+      method: "DELETE",
+    });
+    fetchTodo();
+  };
 
   useEffect(() => {
-    fetch("/api")
-      .then((response) => response.json())
-      .then((result) => {
-        setData(result);
-      });
+    fetchTodo();
   }, []);
 
   return (
     <>
-      <div>
-        <h1>Heeeesaaaaanananananj</h1>
-        <p>{data.hello}</p>
+      <div className="bg-white text-black">
+        <h1 className="text-black">ToDo List</h1>
+
+        <div>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder="Add task"
+          />
+          <button onClick={addTodo}>Add</button>
+        </div>
+        <ul>
+          {todos.map((todo, todoIndex) => (
+            <li key={todoIndex}>
+              {todo}
+              <div>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleTodo(todo.id, todo.completed)}
+                />
+              </div>
+              <span>{todo.title}</span>
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
